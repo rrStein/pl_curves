4f0a2a3f4edf9ce039c685559fc17154e9d8a550
rrStein <rrstein521@gmail.com>
2020-04-29 13:53:55 +0300

    test run fail

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import pytest
 from pl_curve import run
 import pandas
 import os
@@ -35,3 +36,35 @@ def test_run():
     assert data.loc['Step I', 'Corrected Gini'] == 0.0
     assert data.loc['Step II', 'Gini'] == 0.2
     assert data.loc['Step II', 'Corrected Gini'] == 0.4
+    
+def test_run_fail():
+    f = open("test-input.tsv", "w")
+    f.write("Bin\tStep I\tStep II\n")
+    f.write("219\t0.8\t0.3\n")
+    f.write("218\t0.5\t0.7\n")
+    f.close()
+
+    # delete test.png if it already exists
+    try:
+        os.remove("test.tsv")
+    # catch the error if file doesn't exist
+    except FileNotFoundError as error:
+        pass
+
+    # delete test.png if it already exists
+    try:
+        os.remove("test.png")
+    # catch the error if file doesn't exist
+    except FileNotFoundError as error:
+        pass
+
+    
+    # 
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        run("test-input.tsv", "test.png", "test.tsv")
+        
+        captured = capsys.readouterr()
+        errMsg = "Error: columns don't sum to 1\n"
+        assert captured == errMsg
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 1
